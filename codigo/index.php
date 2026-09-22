f076b90a46cb9bc4469255c2b002fe5bc25c7b5a
jean <jean.rodrigues@estudante.ifgoiano.edu.br>
2026-09-22 09:52:48 -0300

    index

---
+++ codigo/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Projeto 1</h1>
+
+    <a href="cad_curso.php">Cadastrar Curso</a><br>
+    <a href="cad_professor.php">Cadastrar Professor</a>
+</body>
+</html>
